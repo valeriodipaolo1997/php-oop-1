2e61f9c4ef8768deccd52e6bd6838fecdf0c2b4a
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-19 15:31:13 +0200

    add class movie

--- index.php
+++ index.php
@@ -5,4 +5,25 @@ Bonus 1: Modificare la classe Movie in modo che accetti piú di un genere.
 Bonus 2: Creare un layout completo per stampare a schermo una lista di movies.
  -->
 
- 
+ <?php
+
+class Movie
+{
+    public $title;
+    public $year;
+    public $language;
+    
+
+    function __construct($_title, $_language, $_year)
+    {
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->year = $_year;
+        
+    }
+};
+
+$interstellar = new Movie('Interstellar', 'Inglese', 2014);
+$lucy = new Movie('Lucy', 'Inglese', 2014);
+var_dump($interstellar);
+var_dump($lucy);
