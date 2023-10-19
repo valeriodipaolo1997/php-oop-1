77310f4e9e8dd27025908e3747975113fbc8d90e
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-19 15:10:46 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<!--create un file index.php in cui:
+è definita una classe ‘Movie’ => all’interno della classe sono dichiarate delle variabili d’istanza => all’interno della classe è definito un costruttore => all’interno della classe è definito almeno un metodo
+vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+Bonus 1: Modificare la classe Movie in modo che accetti piú di un genere.
+Bonus 2: Creare un layout completo per stampare a schermo una lista di movies.
+ -->
+
+ 
